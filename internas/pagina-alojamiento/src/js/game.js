b1e7fcba27b25cc5fdaf5caaf42511efff3f0c6d
inproext <ivan.inproext@gmail.com>
2022-08-22 17:16:04 -0500

    creacion paginas internas

---
+++ internas/pagina-alojamiento/src/js/game.js
@@ -0,0 +1,208 @@
+import * as THREE from './libs/three/three.module.js';
+import { OrbitControls } from './libs/three/OrbitControls.js';
+import { RoomEnvironment } from './libs/three/RoomEnvironment.js';
+
+class Game3d {
+    constructor () {
+        let pmremGenerator;
+        let debugHelpers = false;
+
+        this.mixer;
+        this.modelAnimation;
+        this.clock = new THREE.Clock();
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0x000000 );
+        
+        this.eventEndMovement = new Event('endmovement');
+        this.renderer = new THREE.WebGLRenderer({ 
+            antialias: true,
+            canvas: document.getElementById('Lienzo')
+        });
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+        pmremGenerator = new THREE.PMREMGenerator( this.renderer );
+        this.scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
+
+
+        this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.01, 100)
+        this.camera.position.set( 0, 0, -0.0367);
+        this.camera.lookAt(new THREE.Vector3(0,0,0));
+
+        /**CREATE OBJECTS */
+        this.cube = new THREE.Mesh( new THREE.BoxGeometry(), new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true }));
+        this.cube.scale.x = 1;
+        this.cube.scale.y = 1;
+        this.cube.scale.z = 1;
+        this.cube.visible = debugHelpers;
+
+        this.satelite = new THREE.Mesh( new THREE.BoxGeometry(), new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe: true }));
+        this.satelite.scale.x = 0.02;
+        this.satelite.scale.y = 0.02;
+        this.satelite.scale.z = 0.02;
+        this.satelite.visible = debugHelpers;
+
+        this.satelite2 = new THREE.Mesh( new THREE.BoxGeometry(), new THREE.MeshBasicMaterial( { color: 0x0000ff, wireframe: true }));
+        this.satelite2.scale.x = 0.02;
+        this.satelite2.scale.y = 0.02;
+        this.satelite2.scale.z = 0.02;
+        this.satelite2.visible = debugHelpers;
+
+
+        let particlesGeometry = new THREE.BufferGeometry();
+        let count = 0;  //this.stars 500000;
+        let positions = new Float32Array(count * 3);
+        let colors = new Float32Array(count * 3);
+
+        for(let i = 0; i < count * 3; i++) {
+            positions[i] = (Math.random() - 0.5) * 10;
+            colors[i] = Math.random();
+        }
+
+        particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+        particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+        let textureLoader = new THREE.TextureLoader();
+        let particleTexture = textureLoader.load('/dist/img/particle/star_06.png');
+        let particlesMaterial = new THREE.PointsMaterial();
+        particlesMaterial.size = 0.04;
+        particlesMaterial.sizeAttenuation = true;
+        //particlesMaterial.color = new THREE.Color('#ff0');
+        particlesMaterial.transparent = true;
+        particlesMaterial.alphaMap = particleTexture;
+        //particlesMaterial.alphaTest = 0.001;
+        //particlesMaterial.depthTest = false;
+        particlesMaterial.depthWrite = false;
+        particlesMaterial.blending = THREE.AdditiveBlending;
+        particlesMaterial.vertexColors = true;
+        
+
+        this.particle = new THREE.Points(particlesGeometry, particlesMaterial);
+        this.scene.add(this.particle);
+
+        this.scene.add(this.cube);
+        this.scene.add(this.satelite);
+        this.scene.add(this.satelite2);
+        
+        this.controls();
+    }
+
+    controls() {
+        let _this = this;
+        this.ctrl = new OrbitControls(this.camera, this.renderer.domElement);
+        this.ctrl.target.set(0,0.5,0);
+        this.ctrl.update();
+        this.ctrl.enablePan = true;
+        this.ctrl.enableDamping = true;
+        this.ctrl.addEventListener('end', function() {
+            //_this.camera.lookAt(_this.cube.position);
+        });
+       
+    }
+
+    moveCamera(_point){
+        let _this = this;
+
+        let geometry = this.model.getObjectByName(_point.geometry);
+        let child = this.model.getObjectByName(_point.satelite);
+        let originalPoint = {
+            posX: this.camera.position.x,
+            posY: this.camera.position.y,
+            posZ: this.camera.position.z,
+            color: '#'+this.scene.background.getHexString(),
+        }
+        this.satelite.position.x = child.position.x;
+        this.satelite.position.y = child.position.y;
+        this.satelite.position.z = child.position.z;
+        
+        this.satelite2.position.x = child.position.x;
+        this.satelite2.position.y = child.position.y;
+        if(_point.id === 1) {
+            this.playAnimation();
+            this.satelite2.position.z = child.position.z > 0 ? child.position.z + 0.2 : child.position.z - 0.2;
+        }
+        else{
+            this.stopAnimation();
+            this.satelite2.position.z = child.position.z;
+        }
+
+        anime({
+            targets: originalPoint,
+            posX: _this.satelite2.position.x,
+            posY: _this.satelite2.position.y,
+            posZ: _this.satelite2.position.z,
+            color: _point.bgColor,
+            easing: 'easeOutSine',
+            duration: 800,
+            update: function() {
+                _this.camera.position.x = originalPoint.posX;
+                _this.camera.position.y = originalPoint.posY;
+                _this.camera.position.z = originalPoint.posZ;
+                _this.scene.background = new THREE.Color( originalPoint.color);
+                _this.camera.lookAt(geometry.position);
+            },
+            complete: function() {
+                dispatchEvent(_this.eventEndMovement);
+            }
+        });
+    }
+
+    addModel(_model, _type) {
+        if(_type == 'gltf') {
+            this.model = _model.scene;
+            this.modelAnimation = _model;
+            this.model.position.set(0,0,0);
+            this.model.rotation.set(0,0,0);
+            this.mixer = new THREE.AnimationMixer(this.model);
+            this.modelAnimation.animations.forEach((_item) => {
+                this.mixer.clipAction(_item).play();
+            });
+            this.model.getObjectByName('EsferaCubo').visible = false;
+            this.model.getObjectByName('EsferaEsfera').visible = false;
+            this.model.getObjectByName('OctaedroEsfera').visible = false;
+            this.model.getObjectByName('IcosaedroEsfera').visible = false;
+            this.model.getObjectByName('DodecaedroEsfera').visible = false;
+
+            this.playAnimation();
+        }
+        else {
+            this.model = _model;
+        }
+        this.scene.add(this.model);
+    }
+
+    playAnimation() {
+        this.modelAnimation.animations.forEach((_item) => {
+            this.mixer.clipAction(_item).paused = false;
+        });
+    }
+
+    stopAnimation() {
+        this.modelAnimation.animations.forEach((_item) => {
+            this.mixer.clipAction(_item).paused = true;
+        });
+    }
+
+
+    resize() {
+        console.log(window.innerWidth, window.innerHeight);
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+    
+    update(_obj) {
+        let delta = this.clock.getDelta();
+        let elapsedTime = this.clock.getElapsedTime();
+
+        this.particle.rotation.y = elapsedTime * 0.02;
+
+        this.mixer.update(delta);
+        this.renderer.render(this.scene,this.camera);
+    }
+}
+
+export { Game3d }
---
+++ internas/pagina-videogame/src/js/main.js
@@ -0,0 +1,363 @@
+import { LoaderFiles } from './loaderfiles.js';
+import { Game3d } from "./game.js";
+import { LiquidButton } from './liquidbutton.js';
+import { utils } from './utils';
+
+const activeModel = false;
+const game3d = new Game3d();
+const preload = new LoaderFiles();
+
+let bd = document.body;
+let layerMain = document.getElementById('LayerMain');
+let scrollBar = document.querySelector('.scroll-bar .bar');
+let layerContent = document.getElementById('LayerContent');
+let layerPreload = document.getElementById('LayerPreload');
+let btnMenuMovile = document.querySelector('.btn-menu-movile');
+let menuList = document.querySelector('.menu-list');
+let menuItem = document.querySelectorAll('.menu-item');
+
+let points = { mouseX: 0, mouseY: 0 };
+let currentPosition = 0,
+    lastPosition = -1;
+
+let slideDirection = '';
+let scrollDirection = '';
+
+let cameraPoints = [
+    { id: 1, satelite: 'dodecaedro_centro2', geometry: 'dodecaedro_centro2', bgColor: '#E7CDF9', speed: 2000 },
+    { id: 2, satelite: 'EsferaCubo', geometry: 'cubo', bgColor: '#E7CDF9', speed: 1000 },  
+    { id: 3, satelite: 'EsferaEsfera', geometry: 'esfera', bgColor: '#E7CDF9',  speed: 1000 },
+    { id: 4, satelite: 'OctaedroEsfera', geometry: 'octaedro', bgColor: '#E7CDF9', speed: 1000 }
+];
+
+preload.addContents([
+	{ id: '1', src: 'dist/html/1.html' },
+    { id: '2', src: 'dist/html/2.html' },
+    { id: '3', src: 'dist/html/3.html' },
+    { id: '4', src: 'dist/html/4.html' }
+]);
+
+preload.addImages([
+    { id: 'logo', src: 'dist/img/logo.png' },
+    { id: 'arrow-left', src: 'dist/img/arrow-left.png' },
+    { id: 'arrow-right', src: 'dist/img/arrow-right.png' },
+    { id: 'videogame_1a', src: 'dist/img/videogame_1a.png' },
+    { id: 'videogame_1b', src: 'dist/img/videogame_1b.png' },
+    { id: 'videogame_2a', src: 'dist/img/videogame_2a.png' },
+    { id: 'videogame_3a', src: 'dist/img/videogame_3a.png' },
+    { id: 'videogame_3b', src: 'dist/img/videogame_3b.png' },
+    { id: 'videogame_4a', src: 'dist/img/videogame_4a.png' },
+    { id: 'videogame_4b', src: 'dist/img/videogame_4b.png' }
+
+]);
+
+// preload.addModels([
+// 	{ id: 'modelo1', path: 'models/' , src: 'sistema.gltf', type: 'gltf' }
+// ]);
+
+preload.addSounds([
+
+]);
+
+
+preload.load(function (_progress) {
+	layerPreload.innerHTML = '<h1>'+_progress + '%</h1>';
+}).then(function () {
+    init();
+    anime({
+        targets: layerPreload,
+        opacity: 0,
+        top: "100%",
+        easing: 'easeInQuad',
+        duration: 400,
+        delay: 600,
+        complete: function() {
+            layerPreload.style.display = 'none';
+        }
+    });
+});
+
+
+function init() {
+    setEvents();
+    if(activeModel)
+    {
+        game3d.addModel(Library.models['modelo1'], 'gltf');
+        game3d.resize();
+    }
+    loop();
+    fn_load();
+}
+
+function fn_load(){
+    let scrollProgress = ((currentPosition + 1) / cameraPoints.length * 100);
+    scrollDirection = currentPosition > lastPosition ? "down": "up";
+    if(currentPosition !== lastPosition) {
+        layerContent.innerHTML = '';
+        //layerMain.style.display = 'none';
+        if(activeModel) game3d.moveCamera(cameraPoints[currentPosition]);
+        layerContent.innerHTML = Library.contents[currentPosition+1];
+        checkScrollDirection();
+        checkLiquidButton();
+        checkParallax();
+        document.body.setAttribute('data-page', currentPosition);
+        scrollBar.style.width = scrollProgress + '%';
+        utils.loadImages();
+        utils.parseScript(Library.contents[currentPosition+1]);
+        lastPosition = currentPosition;
+    }
+    else {
+        scrollDirection = "keep";
+    }
+}
+
+function setEvents() {
+    let prevTime = new Date().getTime();
+    let mc = new Hammer(bd);
+    
+    mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });   
+    mc.on('panleft panright panup pandown tap press', function(ev) {
+        let curTime = new Date().getTime();
+        let timeDiff = curTime-prevTime;
+        prevTime = curTime;
+        if(timeDiff > 200) {
+            let scrollContainer = document.querySelector('.scroll');
+
+            if(ev.type == "panup"){
+                slideDirection = 'down';
+                currentPosition += 1;
+            }
+
+            if(ev.type == "pandown"){
+                slideDirection = 'up';
+                currentPosition -= 1;
+            }
+
+            if(scrollContainer == null) {
+                checkArrows();
+                fn_load();                 
+            }
+            
+            checkCarrousel(slideDirection);
+        }
+    });
+    
+    Hamster(bd).wheel(function(event, delta, deltaX, deltaY) {
+        
+        let curTime = new Date().getTime();
+        let timeDiff = curTime-prevTime;
+        prevTime = curTime;
+        if(timeDiff > 200) {
+            let scrollContainer = document.querySelector('.scroll');
+            if(delta == -1){
+                slideDirection = 'down';
+                currentPosition += 1;
+            }
+            else {
+                slideDirection = 'up';
+                currentPosition -= 1;
+            }
+
+            if(scrollContainer == null) {
+                checkArrows();
+                fn_load();                 
+            }
+            
+            checkCarrousel(slideDirection);
+        }
+
+    });
+
+
+    btnMenuMovile.addEventListener('click', function() {
+        menuList.classList.add('active');
+    });
+
+    menuItem.forEach((_item) => {
+        /*
+        _item.addEventListener('click', function() {
+            let pos = parseInt(this.getAttribute('data-html'));
+            currentPosition = pos;
+            menuList.classList.remove('active');
+            deactiveBullets();
+            this.classList.add('active');
+            fn_load();
+        });
+        */
+    });
+
+
+    window.addEventListener('resize', function() {
+        if(activeModel) game3d.resize();
+    });
+
+    window.addEventListener('endmovement', function() {
+        console.log('eventomoviemiento');
+    });
+
+    document.body.addEventListener('mousemove', (_ev) => {
+        try 
+        {
+            let imageReveal = document.querySelector('.front-image');
+            let glass = document.querySelector('.glass');
+            if(imageReveal !== null){
+                _ev.preventDefault();
+                let gx, gy, gw, gh, gscale;
+                let bx, by;
+                let hiddenImage = imageReveal.getAttribute('data-hiddenImage');
+                let srcHiddenImage = Library.images[hiddenImage].src;
+                let rects = imageReveal.getBoundingClientRect();
+                gw = glass.offsetWidth / 2;
+                gh = glass.offsetHeight / 2;
+                gx = _ev.clientX - gw;
+                gy = _ev.clientY - gh;
+                gx = gx - window.pageXOffset;
+                gy = gy - window.pageYOffset;
+                bx = gx - rects.x;
+                by = gy - rects.y;
+                if(gx > rects.x && gx < (rects.width + rects.x - glass.offsetWidth) && gy > rects.y && gy < (rects.height + rects.y - glass.offsetHeight))
+                {
+                    glass.style.opacity = 1;
+                    
+                    gscale = 1;
+                   
+                }
+                else {
+                    glass.style.opacity = 0;
+                    gscale = 0;
+                }
+
+                //glass.pxstyle.transform = 'translate('+gx+'px,'+gy+'px) scale('+gscale+')';
+                glass.style.transform = 'scale('+gscale+')';
+                glass.style.left = gx + 'px';
+                glass.style.top = gy + 'px';
+                glass.style.backgroundImage = 'url('+srcHiddenImage+')';
+                glass.style.backgroundRepeat = 'no-repeat';
+                glass.style.backgroundSize = (rects.width) + "px " + (rects.height) + "px";
+                glass.style.backgroundPosition = "-" + (bx) + "px -" + (by) + "px";
+            }   
+        }
+        catch(err) {
+            console.log(err);
+        } 
+    });
+}
+
+function checkArrows() {
+    if(currentPosition <= 0) {
+        currentPosition = 0;
+    }
+
+    if(currentPosition >= cameraPoints.length - 1) {
+        currentPosition = cameraPoints.length - 1;
+    }
+    
+    //deactiveBullets();
+    //menuItem[currentPosition].classList.add('active');
+}
+
+function checkScrollDirection() {
+    let scroll = document.querySelector('.scroll');
+    let slides = document.querySelectorAll('.slide');
+    let cantSlides = slides.length - 1;
+    if(scroll !== null) {
+        if(scrollDirection == "down") {
+            slides[0].classList.add('active');
+            scroll.setAttribute('data-pos', 0);
+        }
+        if(scrollDirection == "up") {
+            slides[cantSlides].classList.add('active');
+            scroll.setAttribute('data-pos', cantSlides);
+        }
+    }
+}
+
+function checkCarrousel(_slideDirection) {
+    let carrousel = document.querySelector('.carrousel.scroll');
+    let slides = document.querySelectorAll('.slide');
+    if(carrousel !== null) {
+        let init = parseInt(carrousel.getAttribute('data-init'));
+        let pos = parseInt(carrousel.getAttribute('data-pos'));
+        let cantSlides = slides.length - 1;
+
+        if(init) {
+            carrousel.setAttribute('data-init', 0);
+        }
+        else {
+            if(pos == 0) {
+                if( _slideDirection == "up")
+                {
+                    console.log('Cambiar Anterior Pagina');
+                    checkArrows();
+                    fn_load();
+                }
+                else {
+                    currentPosition -= 1;
+                    console.log("Cambiar Siguiente Slide");
+                    carrousel.setAttribute('data-pos', (pos + 1));
+                    slides.forEach(_slide => { _slide.classList.remove('active'); })
+                    slides[pos + 1].classList.add('active');
+                }
+            }
+            else if(pos == cantSlides) {
+                if( _slideDirection == "down")
+                {
+                    console.log("CambiarSiguientePagina");
+                    checkArrows();
+                    fn_load();
+                }
+                else {
+                    currentPosition += 1;
+                    console.log("Cambiar Anterior Slide");
+                    carrousel.setAttribute('data-pos', (pos - 1));
+                    slides.forEach(_slide => { _slide.classList.remove('active'); })
+                    slides[pos - 1].classList.add('active');
+                }
+            }
+            else {
+                if( _slideDirection == "down")
+                {
+                    currentPosition -= 1;
+                    console.log("Cambiar Anterior Slide");
+                    carrousel.setAttribute('data-pos', (pos + 1));
+                    slides.forEach(_slide => { _slide.classList.remove('active'); })
+                    slides[pos + 1].classList.add('active');
+                }
+
+                if( _slideDirection == "up")
+                {
+                    currentPosition += 1;
+                    console.log("Cambiar Anterior Slide");
+                    carrousel.setAttribute('data-pos', (pos - 1));
+                    slides.forEach(_slide => { _slide.classList.remove('active'); })
+                    slides[pos - 1].classList.add('active');
+                }
+
+            }
+        }
+    }
+}
+
+function checkLiquidButton() {
+    let buttons = document.getElementsByClassName("liquid-button");
+    for (let buttonIndex = 0; buttonIndex < buttons.length; buttonIndex++) {
+        let button = buttons[buttonIndex];
+        button.liquidButton = new LiquidButton(button);
+    }
+}
+
+function checkParallax() {
+    let parallaxScene = document.querySelector('.parallax-scene');
+    if(parallaxScene !== null) {
+        new Parallax(parallaxScene);
+    }
+}
+
+function deactiveBullets () {
+    menuItem.forEach(_el => _el.classList.remove('active'));
+}
+
+function loop() {
+    requestAnimationFrame(loop);
+    if(activeModel) game3d.update(points);
+}
